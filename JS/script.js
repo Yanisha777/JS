5dab53a94b495477b37f764e52a7258666fdb3f2
Yanina <yanitasheremeta@gmail.com>
2021-09-20 13:57:33 +0300

    mod function

--- JS/script.js
+++ JS/script.js
@@ -8,12 +8,40 @@ const personalMovieDB = {
 	privat: false,
 };
 
-const a = prompt("One of the last watched movies?", "");
-b = prompt("how much would you rate it?", "");
-c = prompt("One of the last watched movies?", "");
-d = prompt("how much would you rate it?", "");
+for (let i = 0; i < 2; i++) {
+	const a = prompt("One of the last watched movies?", "");
+	b = prompt("how much would you rate it?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log("done");
+	} else {
+		console.log("error");
+		i--;
+	}
+}
 
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+	console.log("you have seen quite a few films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("you are a classic spectator");
+} else if (personalMovieDB.count >= 30) {
+	console.log("you are a movie fan");
+} else {
+	console.log("something goes wrong");
+}
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Your favorite film on number ${i}`);
+	}
+}
+writeYourGenres();
